94fd67d8e439ad129d7a649c092b31549cf08b02
Cam MacDougall <cammacdougall12@gmail.com>
2021-05-24 23:01:15 +0000

    added tail.js

---
+++ tail.js
@@ -0,0 +1,33 @@
+//function to determine tail of array
+const tail = function(array) {
+  let myArray = [];
+
+  for (let i = 1; i < array.length; i++) {
+    myArray.push(array[i]);
+  }
+
+  return myArray;
+};
+
+//check to see if it equals given condition
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ ✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    return console.log(`❌❌❌❌ ${actual} !== ${expected}`);
+  }
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+//Test case for the returned array elements
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
