9323044cbec1828b81b1ae42e47eaac5bce81afa
Cam MacDougall <cammacdougall12@gmail.com>
2021-05-24 21:52:28 +0000

    added assertEquals program

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("✅ ✅ ✅ ✅ Assertion Passed: " + actual + " === " + expected);
+
+  } else {
+    return console.log("❌❌❌❌ actual" + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+
