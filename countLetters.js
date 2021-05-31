9c39b9de705bcbd8d1ab5bca7d4ee7f3091ade5d
Cam MacDougall <cammacdougall12@gmail.com>
2021-05-31 19:59:18 +0000

    refactored countLetters to test outputs using mocha/chai. Added the test file countLettersTest

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,3 @@
-// Assert Equals
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-    return console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-};
-
 //function will count how many time a letter appears in a string
 const countLetters = function(string) {
   const count = {};
@@ -23,17 +13,5 @@ const countLetters = function(string) {
   return count;
 };
 
-let result = countLetters("lighthouse in the house");
-
-//Check result with assert equals
-assertEqual(result.l, 1);
-assertEqual(result.i, 2);
-assertEqual(result.g, 1);
-assertEqual(result.h, 4);
-assertEqual(result.t, 2);
-assertEqual(result.o, 2);
-assertEqual(result.u, 2);
-assertEqual(result.s, 2);
-assertEqual(result.e, 3);
-assertEqual(result.n, 1);
+module.exports = countLetters;
 
---
+++ test/countLettersTest.js
@@ -0,0 +1,56 @@
+//import 
+const assert = require('chai').assert;
+const countLetters = require('../countLetters');
+
+let result = countLetters("lighthouse in the house");
+
+
+//test countLetteres function in countLetters.js
+describe("#head", () => {
+  //test string
+  let result = countLetters("lighthouse in the house");
+  
+  //now see if result outputs correct quantity for each letter
+  it("l appears once in the string 'lighthouse in the house'. Should output 1", () => {
+    assert.strictEqual(result.l, 1);
+  });
+
+  it("i appears twice in the string 'lighthouse in the house'. Should output 2", () => {
+    assert.strictEqual(result.i, 2);
+  });
+
+  it("g appears once in the string 'lighthouse in the house'. Should output 1", () => {
+    assert.strictEqual(result.g, 1);
+  });
+
+  it("h appears 4 times in the string 'lighthouse in the house'. Should output 4", () => {
+    assert.strictEqual(result.h, 4);
+  });
+
+  it("t appears once in the string 'lighthouse in the house'. Should output 1", () => {
+    assert.strictEqual(result.t, 2);
+  });
+
+  it("o appears twice in the string 'lighthouse in the house'. Should output 2", () => {
+    assert.strictEqual(result.o, 2);
+  });
+
+  it("u appears twice in the string 'lighthouse in the house'. Should output 2", () => {
+    assert.strictEqual(result.u, 2);
+  });
+
+  it("s appears twice in the string 'lighthouse in the house'. Should output 2", () => {
+    assert.strictEqual(result.s, 2);
+  });
+
+
+  it("e appears 3 times in the string 'lighthouse in the house'. Should output 3", () => {
+    assert.strictEqual(result.e, 3);
+  });
+
+  it("n appears once in the string 'lighthouse in the house'. Should output 1", () => {
+    assert.strictEqual(result.n, 1);
+  });
+
+});
+
