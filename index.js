28ea0dc5c0003252a0b1a9f0c65d6aea72061e55
Cam MacDougall <cammacdougall12@gmail.com>
2021-05-31 22:16:57 +0000

    modified index.js, so that function are sorted

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 // index.js
-const head = require('./head');
-const tail = require('./tail');
-const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters')
 const countOnly = require('./countOnly')
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-const letterPositions = require('./letterPositions');
 const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const head = require('./head');
 const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
+
+
 const eqArrays = require('./eqArrays');
 
 module.exports = {
